1d78fa814f35961f2ef0215cea4120a6f72a4996
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2022-03-28 10:52:33 -0300

    Add files via upload

---
+++ config/upload.js
@@ -0,0 +1,20 @@
+//importar o pacote multer
+const multer = require('multer')
+
+//configurar o padrao de armazenamento
+const armazenamento = multer.diskStorage(
+    //pasta de destino
+    {
+        destination:(req,file,cb)=>{
+            cb(null,'./assets/images/')
+        },
+        //definir o padrão para os nomes dos arquivos
+        filename:(req,file,cb)=>{
+            cb(null,Date.now()+file.originalname)
+        }
+    }
+)
+
+var upload = multer({storage:armazenamento})
+
+module.exports = upload
